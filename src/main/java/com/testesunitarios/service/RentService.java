5e39ed2336a3867b423c510d3eede3e49941f776
Marcus Túlio <zMarcust.st@hotmail.com>
2022-12-14 14:23:17 -0300

    Ajuste na organização

--- src/main/java/com/testesunitarios/service/RentService.java
+++ src/main/java/com/testesunitarios/service/RentService.java
@@ -28,27 +28,4 @@ public class RentService {
 
         return rentResponse;
     }
-
-    @Test
-    public void test() {
-        //Principio First
-
-        //cenario
-        var rentServiceResponse = new RentService();
-        var userResponse = new UserModel("Marcus");
-        var movieResponse = new MovieModel("Us", 5, 90.00);
-
-        //acao
-        RentModel rentModelResponse = rentServiceResponse.rentMovie(userResponse, movieResponse);
-
-        //verificacao
-        try {
-            Assert.assertTrue(Objects.equals(rentModelResponse.getRentDate(), LocalDate.now()));
-            System.out.println(rentModelResponse);
-        } catch (AssertionError e) {
-            throw new AssertionError("Data do aluguel nao coincide com a data atual!");
-        }
-
-
-    }
 }
---
+++ src/test/java/com/testesunitarios/serviceteste/RentServiceTeste.java
@@ -0,0 +1,35 @@
+package com.testesunitarios.serviceteste;
+
+import com.testesunitarios.model.MovieModel;
+import com.testesunitarios.model.RentModel;
+import com.testesunitarios.model.UserModel;
+import com.testesunitarios.service.RentService;
+import org.junit.Assert;
+import org.junit.Test;
+
+import java.time.LocalDate;
+import java.util.Objects;
+
+public class RentServiceTeste {
+
+    @Test
+    public void test() {
+        //Principio First
+
+        //cenario
+        var rentServiceResponse = new RentService();
+        var userResponse = new UserModel("Marcus");
+        var movieResponse = new MovieModel("Us", 5, 90.00);
+
+        //acao
+        RentModel rentModelResponse = rentServiceResponse.rentMovie(userResponse, movieResponse);
+
+        //verificacao
+        try {
+            Assert.assertTrue(Objects.equals(rentModelResponse.getRentDate(), LocalDate.now()));
+            System.out.println(rentModelResponse);
+        } catch (AssertionError e) {
+            throw new AssertionError("Data do aluguel nao coincide com a data atual!");
+        }
+    }
+}
