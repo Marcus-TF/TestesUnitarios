9f7d37d493cb67d365c5b7d5a11b0c57eee2b139
Marcus Túlio <zMarcust.st@hotmail.com>
2022-12-19 17:08:27 -0300

    Error Collector e ajustes

--- src/test/java/com/testesunitarios/serviceteste/RentServiceTesteII.java
+++ src/test/java/com/testesunitarios/serviceteste/RentServiceTesteII.java
@@ -5,7 +5,6 @@ import com.testesunitarios.model.RentModel;
 import com.testesunitarios.model.UserModel;
 import com.testesunitarios.service.RentService;
 import org.hamcrest.CoreMatchers;
-import org.junit.Assert;
 import org.junit.Rule;
 import org.junit.Test;
 import org.junit.rules.ErrorCollector;
@@ -13,8 +12,7 @@ import org.junit.rules.ErrorCollector;
 import java.time.LocalDate;
 import java.util.Objects;
 
-public class RentServiceTeste {
-
+public class RentServiceTesteII {
     @Rule
     public ErrorCollector error = new ErrorCollector();
     @Test
@@ -31,9 +29,10 @@ public class RentServiceTeste {
 
         //verificacao
         try {
-            Assert.assertEquals(rentModelResponse.getRentDate(), LocalDate.now());
-            Assert.assertTrue(Objects.equals(rentModelResponse.getRentDate(), LocalDate.now()));
-            Assert.assertThat(rentModelResponse.getValue(), CoreMatchers.is(90.00));
+            error.equals(Objects.equals(rentModelResponse.getRentDate(), LocalDate.now())); //Verificando se as datas são iguais com Equals
+            error.checkThat(rentModelResponse.getRentDate(), CoreMatchers.is(LocalDate.now())); //Verificando se as datas são iguais com CheckThat
+//            error.checkThat(rentModelResponse.getValue(), CoreMatchers.is(80.00)); Gerando erro
+            error.checkThat(rentModelResponse.getValue(), CoreMatchers.is(90.00)); //Verificando se os valores são iguais
             System.out.println("Test passed!\n" + rentModelResponse);
         } catch (AssertionError e) {
             throw new AssertionError("Data do aluguel nao coincide com a data atual!");
